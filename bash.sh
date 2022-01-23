90897537ad66fc9bab654b8d76e28271f2b24c25
sanghyeok <vlek1993@gmail.com>
2022-01-24 01:42:20 +0900

    init commit

--- bash.sh
+++ bash.sh
@@ -1,1 +1,2 @@
 python -m torch.distributed.launch --nproc_per_node=4 tools/train.py --cfg experiments/cityscapes/hrnet_w18_v1.yaml
+#python tools/test.py --cfg experiments/cityscapes/hrnet_w18_v1.yaml TEST.MODEL_FILE 'output/cityscapes/hrnet_w18_v1/best.pth'
---
+++ conda_dropclass.sh
@@ -0,0 +1,13 @@
+# Install python dependencies
+conda install install nb_pip 
+pip install numpy 
+pip install cython
+pip install jupyter
+pip install matplotlib
+pip install nltk
+pip install tqdm
+conda install pytorch==0.4.1
+pip install torchvision==0.2.1
+pip install cuda80
+pip install opencv
+pip install tensorflow
